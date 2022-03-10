1074c293e9ac5553a3820304dbec0ed08fac8c99
liuzx <1252288907@qq.com>
2022-03-10 18:12:31 +0800

    第二次进行数据的提交。。。

--- TestServlet1/src/com/lzx/servlet/servlet1.java
+++ TestServlet1/src/com/lzx/servlet/servlet1.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 
 public class servlet1 extends HttpServlet {
+    int hhh = 0;
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("servlet1 service invoked");
+        System.out.println(hhh);
     }
 }
